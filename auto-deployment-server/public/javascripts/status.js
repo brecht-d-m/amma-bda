ee19c03be42d3298e10695c7b7861b7a9dc346ac
Frédérique <fred.Debaerdemaeker@gmail.com>
2016-05-15 17:12:22 +0200

    disable button when datalab is down

--- auto-deployment-server/public/javascripts/status.js
+++ auto-deployment-server/public/javascripts/status.js
@@ -4,20 +4,23 @@
 var socket = io.connect('http://localhost:3000');
 
 socket.on('status', function (data) {
-    updateStatusLabel(document.getElementById("local-status"), data.local);
-    updateStatusLabel(document.getElementById("cloud-status"), data.cloud);
+    updateStatusLabel(document.getElementById("local-status"), data.local, $("#retrieve-local-notebooks"));
+    updateStatusLabel(document.getElementById("cloud-status"), data.cloud, $("#retrieve-cloud-notebooks"));
 
 });
 
-function updateStatusLabel(label, data){
+function updateStatusLabel(label, data, button){
     if (data == "ok"){
         label.textContent = "Ok";
         label.className = "navbar-inner badge badge-success";
+        button.prop('disabled', false);
     } else if (data =="?") {
         label.textContent = "?";
         label.className = "badge badge-warning";
+        button.prop('disabled', true);
     } else {
         label.textContent = "Down";
         label.className = "badge badge-danger";
+        button.prop('disabled', true);
     }
 }
